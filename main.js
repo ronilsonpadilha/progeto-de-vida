0603edfdf127016ead9069654ef231af4a5e03d2
R0N1S0N <162499213+ronilsonpadilha@users.noreply.github.com>
2024-04-19 15:27:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ console.log(botoes)
 for(let i=0; i <  botoes.length; i++){
     console.log(i);
 }
-botoes[i].onclick = function(){
+botoes[i].onclick = function(){}
+    for(let j=0;j<botoes.length;j++){
+        botoes[j].classList.remove("ativo");
     
     botoes[i].classList.add("ativo");
 }
+  
 
